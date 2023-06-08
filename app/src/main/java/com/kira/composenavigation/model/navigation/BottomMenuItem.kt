78e5461c51899922c2ff23f19a782473565d4b6a
kira <kiravillagracia@yahoo.com>
2023-06-08 12:25:43 +0800

    added model for bottom navigation menu

---
+++ app/src/main/java/com/kira/composenavigation/model/navigation/BottomMenuItem.kt
@@ -0,0 +1,15 @@
+package com.kira.composenavigation.model.navigation
+
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Email
+import androidx.compose.material.icons.filled.Home
+import androidx.compose.material.icons.filled.List
+import androidx.compose.material.icons.filled.Person
+import androidx.compose.ui.graphics.vector.ImageVector
+
+sealed class BottomMenuItem(val label: String, val icon: ImageVector, val screenRoute: String) {
+    object Home : BottomMenuItem("Home", Icons.Filled.Home, "home")
+    object Account : BottomMenuItem("Account", Icons.Filled.Person, "account")
+    object Mail : BottomMenuItem("Mail", Icons.Filled.Email, "mail")
+    object Shop : BottomMenuItem("Shop", Icons.Filled.List, "shop")
+}
